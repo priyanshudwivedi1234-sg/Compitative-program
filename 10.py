94a640c3a8bb302798e66f5a6d153e52a3628040
Priyanshu  Dwivedi <priyanshudwivedi7000@gmail.com>
2025-08-28 22:52:38 +0530

    Add files via upload

---
+++ 10.py
@@ -0,0 +1,5 @@
+A = input("Enter a number: ")
+if A == A[::-1]:
+    print("Yes")
+else:
+    print("No")
---
+++ 9.py
@@ -0,0 +1,5 @@
+N = input("Enter a number: ")
+total = 0
+for digit in N:
+    total += int(digit)
+print("Sum of digits:", total)
